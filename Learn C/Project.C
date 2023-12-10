13fdb31375f23ccc4071812ac70f233a664db43b
DahmanAl9r <110249912+dahmanAl9r@users.noreply.github.com>
2023-12-10 22:07:46 +0100

    Update Project.C

--- Learn C/Project.C
+++ Learn C/Project.C
@@ -1,7 +1,24 @@
 #include <windows.h>
 const char g_szClassName[] = "MyWindowClass";
 int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow){
+    /*الخطوة 4 - اجراء النفاذة*/
+    LRESULT CALLBACK WNDPROC(HWND hwnd,UINT msg, WPARAM wParm,LPARAM lParm){
+        switch(msg){
+            case WM_CLOSE:
+                DestroyWindow(hwnd);
+                break;
+            case WM_DESTROY:
+                PostQuitMessage(0);
+                break;
+            default:
+                return DefWindowProc(hwnd,msg,wParm,lParm);
+        }
+        return 0;
+    }
+
+
     /*تسجيل فئة النافذة-1*/
+
     WNDCLASSEX wc;
     wc.cbSize = sizeof(WNDCLASSEX);
     wc.style = 0;
@@ -20,7 +37,11 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,i
         MessageBox(NULL,"Window Register Failed","Error!",MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
+
+
+
     /*الخطوة 2 : انشاء النافذة*/
+
     HWND hwnd;
     hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,g_szClassName,
                          "The title of my Window",
@@ -31,5 +52,21 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,i
         MessageBox((NULL,"Window Create Failed !","Error!",MB_ICONEXCLAMATION | MB_OK));
         return 0;
     }
+    ShowWindow(hwnd,nCmdShow);
+    
+    UpdateWindow(hwnd);
+
+
+
+    /*الخطوة 3 - حلقة الرسائل*/
+    
+    MSG Msg;
+    while(GetMessage(&Msg,NULL,NULL,0,0 > 0){
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+        return Msg.wParm;
+    }
+
+}
 
 }
